some new optim params, fixed bug with accuracy calculation, updated evaluation

data.py
@@ -32,7 +32,7 @@ def _get_cifar10(train: bool, **kwargs) -> torch.utils.data.DataLoader:
         root=DEFAULT_DATA_DIR,
         train=train,
         download=True,
-        transform=_default_cifar10_transforms() if train else _default_transforms((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transform=_default_cifar10_transforms()
     )
     return torch.utils.data.DataLoader(trainset, shuffle=train, num_workers=os.cpu_count(), **kwargs)
 

models.py
@@ -1,12 +1,10 @@
-from typing import Any
-
 import timm
 import torch
-from torch.nn import functional as F
 from timm.models import register_model, ResNet, Bottleneck
 from timm.models.vision_transformer import VisionTransformer
+from torch.nn import functional as F
 
-from utils import remove_prefix, _get_latest_model_path
+from utils import remove_prefix, get_latest_model_path
 
 
 def get_model(name: str, **kwargs) -> torch.nn.Module:
@@ -16,21 +14,26 @@ def get_model(name: str, **kwargs) -> torch.nn.Module:
     :return:
     """
     with torch.no_grad():
-        return timm.create_model(name, **kwargs)
+        return timm.create_model(name, pretrained_cfg=None, **kwargs)
 
 
-def get_eval_model(name: str, **kwargs) -> torch.nn.Module:
+def get_eval_model(name: str, ckpt_path=None, **kwargs) -> torch.nn.Module:
     """
     Returns a self trained model from the local model directory.
     :return:
     """
     model = get_model(name, **kwargs)
-    ckpt_path = _get_latest_model_path(name)
-    print(f'Loading model from ckpt: {ckpt_path}')
+
+    if not ckpt_path:
+        ckpt_path = get_latest_model_path(name)
+
     ckpt = torch.load(ckpt_path)
 
     # remove prefix due to PL state dict naming
-    model.load_state_dict(remove_prefix(ckpt['state_dict'], 'model.'))
+    if 'dino' in name:
+        model.load_state_dict(remove_prefix(ckpt, 'module.'))
+    else:
+        model.load_state_dict(remove_prefix(ckpt['state_dict'], 'model.'))
     model.eval()
     return model
 
@@ -41,8 +44,6 @@ def vit_tiny_cifar10(pretrained=False, **kwargs):
     ViT-Tiny (Vit-T/8) for CIFAR10 training
     if pretrained = True then returns a Vit-T/16 pretrained on ImageNet with size 224.
     """
-    if pretrained:
-        return timm.create_model('vit_tiny_patch16_224', img_size=32, num_classes=10, pretrained=pretrained, **kwargs)
     return VisionTransformer(img_size=32, patch_size=8, num_classes=10, embed_dim=192, depth=12, num_heads=3, **kwargs)
 
 
@@ -52,6 +53,12 @@ def resnet26_cifar10(pretrained=False, **kwargs):
         raise NotImplementedError('No pretrained ResNets :-(')
 
     return ResNet(block=Bottleneck, layers=[2, 2, 2, 2], num_classes=10, **kwargs)
+
+
+@register_model
+def dino_b_cifar100(pretrained=False, **kwargs):
+    return VisionTransformer(img_size=224, patch_size=16, embed_dim=768, num_heads=12, depth=12, num_classes=100,
+                             **kwargs)
 
 
 @register_model

utils.py
@@ -17,19 +17,15 @@ CIFAR_10_CORRUPTIONS = (
 
 
 def get_experiment_name(args):
-    name = f'{args.model}_{args.epochs}_{args.batch_size}'
+    name = f'{args.model}_e{args.epochs}_b{args.batch_size}_o{args.optimizer}_lr{args.learning_rate:.4f}_wd{args.weight_decay:4f}'
     return name
-
-
-def accuracy(y, y_hat, top_k=1):
-    return torch.eq(y_hat.argmax(-top_k), y).float().mean()
 
 
 def remove_prefix(state_dict, prefix):
     return {k[len(prefix):]: v for k, v in state_dict.items() if k.startswith(prefix)}
 
 
-def _get_latest_model_path(name):
+def get_latest_model_path(name):
     model_version_path = f'{TENSORBOARD_LOG_DIR}/{name}'
     latest_version = sorted(
         os.listdir(model_version_path),
@@ -37,6 +33,7 @@ def _get_latest_model_path(name):
         key=lambda x: int(x.split('_')[1])
     )[0]
     ckpt_path = glob.glob(f'{model_version_path}/{latest_version}/checkpoints/*.ckpt')[0]
+    print(f'Loading model from ckpt: {ckpt_path}')
     return ckpt_path
 
 

visualize.py
@@ -15,6 +15,18 @@ from utils import grad_cam_reshape_transform, attention_viz_forward_wrapper
 
 # https://www.cs.toronto.edu/~kriz/cifar.html
 CIFAR10_LABELS = ('airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+CIFAR100_LABELS = (
+    'apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy',
+    'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock',
+    'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest',
+    'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard',
+    'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid',
+    'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum',
+    'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper',
+    'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone',
+    'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf',
+    'woman', 'worm'
+)
 
 
 def grad_cam(model, model_name, data):
@@ -28,7 +40,7 @@ def grad_cam(model, model_name, data):
     # use only one random image for now
     random_choice = random.randint(0, len(data[0]))
 
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 5))
     fig.suptitle(f'Input image class: {CIFAR10_LABELS[data[1][random_choice]]}')
     fig.tight_layout()
 
@@ -38,6 +50,10 @@ def grad_cam(model, model_name, data):
         target_layer = [model.layer4[-1]]
 
     input_tensor = data[0][random_choice:random_choice + 1]
+
+    y = model(input_tensor)
+    preds = [f'{CIFAR10_LABELS[i]} ({y[0][i]})' for i in y[0].argsort(descending=True)]
+
     cam = GradCAM(
         model=model,
         target_layers=target_layer,
@@ -50,6 +66,14 @@ def grad_cam(model, model_name, data):
 
     visualization = show_cam_on_image(input_tensor, grayscale_cam[0, :], use_rgb=True)
     ax2.imshow(visualization)
+
+    ax3.axis('off')
+    ax3.axis('tight')
+    ax3.table(
+        [[p] for p in preds],
+        colLabels=[f'Top {len(preds)} predictions'],
+        loc='center',
+    )
 
     sub_dir_name = 'grad_cam'
     os.makedirs(f'./plots/{model_name}/{sub_dir_name}', exist_ok=True)
@@ -68,8 +92,6 @@ def dino_attention(model, model_name, data):
     :param model_name:
     :return:
     """
-    if 'vit' not in model_name:
-        raise NotImplementedError('Attention visualization only works for ViT models.')
 
     # use only one random image for now
     random_choice = random.randint(0, len(data[0]))
@@ -79,7 +101,10 @@ def dino_attention(model, model_name, data):
     # use only one image for now
     img = data[0][random_choice:random_choice + 1]
     y = model(img)
-    print('\n'.join([f'{CIFAR10_LABELS[i]} ({y[0][i]})' for i in y[0].argsort(descending=True)]))
+
+    # preds = [f'{CIFAR100_LABELS[i]} ({y[0][i]})' for i in y[0].argsort(descending=True)]
+    preds = [f'{CIFAR10_LABELS[i]} ({y[0][i]})' for i in y[0].argsort(descending=True)]
+    print('\n'.join(preds))
 
     attn_map = model.blocks[-1].attn.attn_map.mean(dim=1).squeeze(0).detach()
     cls_weight = model.blocks[-1].attn.cls_attn_map.mean(dim=1).view(4, 4).detach()
@@ -87,7 +112,7 @@ def dino_attention(model, model_name, data):
     img_resized = img[0].permute(1, 2, 0) * 0.5 + 0.5
     cls_resized = F.interpolate(cls_weight.view(1, 1, 4, 4), (32, 32), mode='bilinear').view(32, 32, 1)
 
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 6))
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(12, 6))
     fig.suptitle(f'Input image class: {CIFAR10_LABELS[data[1][random_choice]]}')
     fig.tight_layout()
 
@@ -102,6 +127,14 @@ def dino_attention(model, model_name, data):
     ax3.set_title('Last Block Attention Map')
     ax3.set_xlabel('Head')
     ax3.set_ylabel('Patch')
+
+    ax4.axis('off')
+    ax4.axis('tight')
+    ax4.table(
+        [[p] for p in preds],
+        colLabels=[f'Top {len(preds)} predictions'],
+        loc='center',
+    )
 
     plt.show()
 